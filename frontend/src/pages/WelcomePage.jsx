6f12f5236017b599d0664eabaa941d3bd944a09a
서만기 <seo_m98@naver.com>
2024-03-18 17:19:10 +0900

    FEAT: 페이지 스켈레톤 코드 추가

---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import Container from "../components/common/Container";
+import styles from "./WelcomePage.module.css";
+
+export default function WelcomePage() {
+  return (
+    <Container>
+      <div className={styles.container}>
+        dfadfgakjgvhldiukvgjehigukerhgierkgvaehugkve
+      </div>
+    </Container>
+  );
+}
