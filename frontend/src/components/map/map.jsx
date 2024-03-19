91011d1705d579c039c95e2619fff1ba4f1d1c64
최준호 <cojuna@naver.com>
2024-03-19 16:15:33 +0900

    FEAT : 지도페이지

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,75 +1,90 @@
 import { useEffect, useRef } from "react";
 import styles from "./map.module.css";
-import { markerdata } from "./data/markerData";
-
+import { markerdataB } from "./data/markerDataB";
+import { markerdataG } from "./data/markerDataG";
 
 function makeInfowindowContent(title) {
-    return `
-        <div style="padding: 10px;">
-            <h2>${title}</h2>
-            <p>서만기바보.</p>
-        </div>
+  return `
+    <div style="padding: 10px; background-color: black; font-size : 1em;">
+        <h2>${title}</h2>
+    </div>
     `;
 }
 
-
 export default function Map() {
-    const { kakao } = window;
-    const mapRef = useRef(null);
+  const { kakao } = window;
+  const mapRef = useRef(null);
 
-    useEffect(() => {
-        mapscript();
-    }, []);
+  useEffect(() => {
+    mapscript();
+  }, []);
 
-    const mapscript = () => {
-        const container = mapRef.current; // mapRef를 통해 요소를 가져옵니다.
-        if (!container) return; // 요소가 없으면 함수를 종료합니다.
+  const mapscript = () => {
+    const container = mapRef.current; // mapRef를 통해 요소를 가져옵니다.
+    if (!container) return; // 요소가 없으면 함수를 종료합니다.
 
-        const options = {
-            center: new kakao.maps.LatLng(36.3573677, 127.346910),
-            level: 4,
-        };
-
-        // 지도 생성
-        const map = new kakao.maps.Map(container, options);
-
-        // 마커 추가
-        markerdata.forEach((el) => {
-            const marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(el.lat, el.lng),
-            });
-            const infowindow = new kakao.maps.InfoWindow({
-                content: makeInfowindowContent(el.title)
-            });
-            kakao.maps.event.addListener(
-                marker,
-                "mouseover",
-                makeOverListener(map, marker, infowindow)
-            );
-            kakao.maps.event.addListener(
-                marker,
-                "mouseout",
-                makeOutListener(infowindow)
-            );
-        });
+    const options = {
+      center: new kakao.maps.LatLng(36.3573677, 127.34691),
+      level: 6,
     };
 
-    // 인포윈도우를 표시하는 클로저를 만드는 함수
-    function makeOverListener(map, marker, infowindow) {
-        return function () {
-            infowindow.open(map, marker);
-        };
-    }
+    // 지도 생성
+    const map = new kakao.maps.Map(container, options);
 
-    // 인포윈도우를 닫는 클로저를 만드는 함수
-    function makeOutListener(infowindow) {
-        return function () {
-            infowindow.close();
-        };
-    }
+    // 마커 추가
+    markerdataB.forEach((el) => {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+      });
+      const infowindow = new kakao.maps.InfoWindow({
+        content: makeInfowindowContent(el.title),
+      });
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
+    });
+    markerdataG.forEach((el) => {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+      });
+      const infowindow = new kakao.maps.InfoWindow({
+        content: makeInfowindowContent(el.title),
+      });
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
+    });
+  };
 
-    return (
-        <div className={styles.map} ref={mapRef}></div>
-    );
+  // 인포윈도우를 표시하는 클로저를 만드는 함수
+  function makeOverListener(map, marker, infowindow) {
+    return function () {
+      infowindow.open(map, marker);
+    };
+  }
+
+  // 인포윈도우를 닫는 클로저를 만드는 함수
+  function makeOutListener(infowindow) {
+    return function () {
+      infowindow.close();
+    };
+  }
+
+  return <div className={styles.map} ref={mapRef}></div>;
 }
