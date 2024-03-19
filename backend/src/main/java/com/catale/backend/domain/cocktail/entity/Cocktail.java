3b67e7bc95e236c1f46c9c640523e26a8191a054
최소영 <tiger6453127@naver.com>
2024-03-19 13:29:55 +0900

    FEAT: entity 컬럼 추가 및 관계 설정 추가

--- backend/src/main/java/com/catale/backend/domain/cocktail/entity/Cocktail.java
+++ backend/src/main/java/com/catale/backend/domain/cocktail/entity/Cocktail.java
@@ -1,4 +1,80 @@
 package com.catale.backend.domain.cocktail.entity;
 
+import com.catale.backend.domain.image.entity.Image;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cocktail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cocktail_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE})
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    private String name;
+
+    @Column(name = "alc")
+    private int alc;
+
+    @Column(name = "sweet")
+    private int sweet;
+
+    @Column(name = "sour")
+    private int sour;
+
+    @Column(name = "bitter")
+    private int bitter;
+
+    @Column(name = "sparking")
+    private int sparking;
+
+    /* 칵테일의 색은 1~9까지로 나뉘어짐,
+    * 1: 핑크 / 2: 빨강 / 3: 주황 / 4: 노랑 / 5: 갈색 / 6: 클리어 / 7: 초록 / 8: 화이트 / 9: 파랑
+    *  */
+    @Column(name = "color1")
+    private int color1;
+
+    @Column(name = "color2")
+    private int color2;
+
+    @Column(name = "color3")
+    private int color3;
+
+    @Column(name = "glass")
+    private int glass;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "ingredient")
+    private String ingredient;
+
+    @Column(name = "base")
+    private int base;
+
+    @Column(name = "emotion1")
+    private int emotion1;
+
+    @Column(name = "emotion2")
+    private int emotion2;
+
+    @Column(name = "emotion3")
+    private int emotion3;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @Column(name = "fruit")
+    private int fruit;
+
+
 }
