c2f418d44b0f61933bab8d0e2a1886d351d47149
유호정 <ys0403ab@naver.com>
2024-03-20 16:41:22 +0900

    FEAT: 다이어리 리스폰스 dto 작성

--- backend/src/main/java/com/catale/backend/domain/diary/dto/DiaryGetResponseDto.java
+++ backend/src/main/java/com/catale/backend/domain/diary/dto/DiaryGetResponseDto.java
@@ -1,4 +1,31 @@
 package com.catale.backend.domain.diary.dto;
 
+import com.catale.backend.domain.diary.entity.Diary;
+import lombok.Data;
+
+@Data
 public class DiaryGetResponseDto {
+    private Long id;
+    private Long memberId;
+    private Long cocktailId;
+    private int mood;
+    private String comment;
+    private String reason;
+    private int month;
+    private int emotion1;
+    private int emotion2;
+    private int emotion3;
+
+    public DiaryGetResponseDto(Diary diary){
+        this.id = diary.getId();
+        this.memberId = diary.getMember().getId();
+        this.cocktailId = diary.getCocktail().getId();
+        this.mood = diary.getMood();
+        this.comment = diary.getComment();
+        this.reason = diary.getReason();
+        this.month = diary.getMonth();
+        this.emotion1 = diary.getEmotion1();
+        this.emotion2 = diary.getEmotion2();
+        this.emotion3 = diary.getEmotion3();
+    }
 }
