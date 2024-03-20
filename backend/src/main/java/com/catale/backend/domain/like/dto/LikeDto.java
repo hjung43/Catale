7833e9f77235c1acda1381ad9e60f84a5154647c
유호정 <ys0403ab@naver.com>
2024-03-20 17:24:38 +0900

    FEAT: 좋아요 dto 작성

--- backend/src/main/java/com/catale/backend/domain/like/dto/LikeDto.java
+++ backend/src/main/java/com/catale/backend/domain/like/dto/LikeDto.java
@@ -1,4 +1,18 @@
 package com.catale.backend.domain.like.dto;
 
+import com.catale.backend.domain.like.entity.Like;
+import lombok.Data;
+
+@Data
 public class LikeDto {
+
+    private Long id;
+    private Long memberId;
+    private Long cocktailId;
+
+    public LikeDto (Like like){
+        this.id = like.getId();
+        this.memberId = like.getMember().getId();
+        this.cocktailId = like.getCocktail().getId();
+    }
 }
