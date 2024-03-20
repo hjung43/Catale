d02d50834d46308e588fae817bcd189c34567e60
유호정 <ys0403ab@naver.com>
2024-03-20 16:41:01 +0900

    FEAT: 다이어리 레포지토리 생성

--- backend/src/main/java/com/catale/backend/domain/diary/repository/DiaryRepository.java
+++ backend/src/main/java/com/catale/backend/domain/diary/repository/DiaryRepository.java
@@ -1,4 +1,12 @@
 package com.catale.backend.domain.diary.repository;
 
-public class DiaryRepository {
+import com.catale.backend.domain.diary.entity.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
 }
