1522b035df41d5d08832377c7c6742bd97880846
서만기 <seo_m98@naver.com>
2024-03-20 10:30:18 +0900

    FEAT: 필요없는 변수 제거

--- frontend/src/components/common/Container.jsx
+++ frontend/src/components/common/Container.jsx
@@ -1,18 +1,8 @@
 import s from "classnames";
 import styles from "./Container.module.css";
 
-function Container({ children, width, height = "100vh" }) {
-  return (
-    <div
-      className={s(styles.container)}
-      style={{
-        width,
-        height,
-      }}
-    >
-      {children}
-    </div>
-  );
+function Container({ children }) {
+  return <div className={s(styles.container)}>{children}</div>;
 }
 
 export default Container;
