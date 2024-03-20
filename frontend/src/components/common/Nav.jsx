f2b0a637c169b33c15c921e1387ff97b79a300f2
서만기 <seo_m98@naver.com>
2024-03-20 10:28:54 +0900

    FEAT: 상단에 주석추가

--- frontend/src/components/common/Nav.jsx
+++ frontend/src/components/common/Nav.jsx
@@ -1,3 +1,6 @@
+// 틀 컴포넌트
+// 네브바 부분에 해당합니다.
+
 import s from "classnames";
 import styles from "./Nav.module.css";
 import calA from "../../assets/icon/calA.png";
@@ -11,20 +14,14 @@ import searchB from "../../assets/icon/searchB.png";
 import userA from "../../assets/icon/userA.png";
 import userB from "../../assets/icon/userB.png";
 
-function Nav({ num, width, height = "7vh" }) {
+function Nav({ num }) {
   return (
-    <div
-      className={s(styles.nav)}
-      style={{
-        width,
-        height,
-      }}
-    >
-      <img src={num === 1 ? searchB : searchA} />
-      <img src={num === 2 ? mapB : mapA} />
-      <img src={num === 3 ? homeB : homeA} />
-      <img src={num === 4 ? calB : calA} />
-      <img src={num === 5 ? userB : userA} />
+    <div className={s(styles.nav)}>
+      <img src={num === 1 ? searchB : searchA} alt="search_icon" />
+      <img src={num === 2 ? mapB : mapA} alt="map_icon" />
+      <img src={num === 3 ? homeB : homeA} alt="home_icon" />
+      <img src={num === 4 ? calB : calA} alt="calendar_icon" />
+      <img src={num === 5 ? userB : userA} alt="user_icon" />
     </div>
   );
 }
