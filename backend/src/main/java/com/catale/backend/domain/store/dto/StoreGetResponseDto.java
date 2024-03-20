a2db36a03525d8d97d946a88c8b23b80d02ba6e5
심규리 <jinry0034@naver.com>
2024-03-20 17:18:48 +0900

    FEAT:가게 도메인 로직 작성

--- backend/src/main/java/com/catale/backend/domain/store/dto/StoreGetResponseDto.java
+++ backend/src/main/java/com/catale/backend/domain/store/dto/StoreGetResponseDto.java
@@ -1,4 +1,29 @@
 package com.catale.backend.domain.store.dto;
 
+import com.catale.backend.domain.store.entity.Store;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
 public class StoreGetResponseDto {
+
+    private Long id;
+    private boolean groupAvailable;
+    private boolean reservationAvailable;
+    private boolean petAvailable;
+    private boolean wifiAvailable;
+    private boolean parkAvailable;
+
+    public StoreGetResponseDto(Store store){
+        this.id = store.getId();
+        this.groupAvailable = store.isGroupAvailable();
+        this.reservationAvailable = store.isReservationAvailable();
+        this.petAvailable = store.isPetAvailable();
+        this.wifiAvailable = store.isWifiAvailable();
+        this.parkAvailable = store.isParkAvailable();
+    }
+
 }
--- backend/src/main/java/com/catale/backend/domain/store/service/StoreService.java
+++ backend/src/main/java/com/catale/backend/domain/store/service/StoreService.java
@@ -1,4 +1,40 @@
 package com.catale.backend.domain.store.service;
 
+import com.catale.backend.domain.store.dto.StoreGetResponseDto;
+import com.catale.backend.domain.store.entity.Store;
+import com.catale.backend.domain.store.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class StoreService {
+
+    @Autowired
+    private final StoreRepository storeRepository;
+
+    //가게 전체 조회
+    @Transactional(readOnly = true)
+    public List<StoreGetResponseDto> StoreFindAll() {
+        List<Store> storeList = storeRepository.findAll();
+        List<StoreGetResponseDto> list = new ArrayList<>();
+        for (Store s : storeList) {
+            list.add(new StoreGetResponseDto(s));
+        }
+        return list;
+    }
+
+    //가게 상세 조회
+    @Transactional(readOnly = true)
+    public StoreGetResponseDto findById(Long id) {
+        Store store = storeRepository.findById(id).orElseThrow(NullPointerException::new);
+        return new StoreGetResponseDto(store);
+    }
+
+
 }
