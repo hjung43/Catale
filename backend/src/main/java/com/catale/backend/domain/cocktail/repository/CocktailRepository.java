0eb6c5b0cdc18fef93ec4fdcb617fcb4d64bd2ec
유호정 <ys0403ab@naver.com>
2024-03-21 16:44:01 +0900

    FEAT: 칵테일 repository 생성

--- backend/src/main/java/com/catale/backend/domain/cocktail/repository/CocktailRepository.java
+++ backend/src/main/java/com/catale/backend/domain/cocktail/repository/CocktailRepository.java
@@ -1,4 +1,9 @@
 package com.catale.backend.domain.cocktail.repository;
 
-public class CocktailRepository {
+import com.catale.backend.domain.cocktail.entity.Cocktail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
 }
