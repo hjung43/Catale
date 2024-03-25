9579421e8a362cb286a0085e83ba7326c7d84583
유호정 <ys0403ab@naver.com>
2024-03-25 16:24:44 +0900

    FEAT: 칵테일 도메인 예외처리

--- backend/src/main/java/com/catale/backend/global/exception/cocktail/CocktailNotFoundException.java
+++ backend/src/main/java/com/catale/backend/global/exception/cocktail/CocktailNotFoundException.java
@@ -2,6 +2,7 @@ package com.catale.backend.global.exception.cocktail;
 
 import com.catale.backend.global.format.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 
 @RequiredArgsConstructor
 public class CocktailNotFoundException extends RuntimeException {
@@ -9,4 +10,5 @@ public class CocktailNotFoundException extends RuntimeException {
     public CocktailNotFoundException() {
         this.errorCode = ErrorCode.COCKTAIL_NOT_FOUND;
     }
+
 }
