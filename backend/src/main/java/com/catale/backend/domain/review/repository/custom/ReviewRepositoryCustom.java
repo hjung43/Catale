908b31b534a241ab783b73d21a8f4c3b5c71652a
유호정 <ys0403ab@naver.com>
2024-03-25 17:56:50 +0900

    FIX: 리뷰리스트 페이징 처리

--- backend/src/main/java/com/catale/backend/domain/review/repository/custom/ReviewRepositoryCustom.java
+++ backend/src/main/java/com/catale/backend/domain/review/repository/custom/ReviewRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.catale.backend.domain.review.repository.custom;
 
 import com.catale.backend.domain.review.dto.ReviewGetResponseDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepositoryCustom {
-    Optional<List<ReviewGetResponseDto>> findByCocktailId(Long CocktailId);
+    Optional<List<ReviewGetResponseDto>> findByCocktailId(Long CocktailId, Pageable page);
 }
