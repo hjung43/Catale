b22a298db388a11562bcc1b46ebc955bc43945e3
고은석 <ko2sist@gmail.com>
2024-03-26 11:18:01 +0900

    FEAT: cors setting

--- backend/src/main/java/com/catale/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/catale/backend/global/config/WebConfig.java
@@ -25,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(corsAllowedOrigins)
+                .allowedOriginPatterns(corsAllowedOrigins)
+//                .allowedOrigins(corsAllowedOrigins)
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true);
     }
