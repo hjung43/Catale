69077919d134494d4dd12cf382473138620ffdad
tiger3127 <tiger6453127@naver.com>
2024-03-27 03:21:43 +0900

    FEAT: 오늘의 칵테일 조회결과 반환 로직 구현중

---
+++ backend/src/main/java/com/catale/backend/domain/cocktail/service/RecommendApiService.java
@@ -0,0 +1,25 @@
+package com.catale.backend.domain.cocktail.service;
+
+import com.catale.backend.domain.cocktail.dto.CocktailListResponseDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class RecommendApiService {
+
+      private final WebClient webClient = WebClient.builder()
+                                                   .baseUrl("https://api.silvstone.xyz:8000/api/v1/recommend")
+                                                   .build();
+
+      public Mono<List<Long>> getApiResponse(Long cocktailId) {
+            return this.webClient.get()
+                                 .uri("/{cocktailId}", cocktailId)
+                                 .retrieve()
+                                 .bodyToMono(new ParameterizedTypeReference<List<Long>>() {});
+      }
+}
