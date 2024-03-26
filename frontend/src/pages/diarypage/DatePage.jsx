8906f395a10c4e014f8afc6d928480f8bec55389
서만기 <seo_m98@naver.com>
2024-03-26 14:53:57 +0900

    FEAT: 날짜상세페이지 작업중

--- frontend/src/pages/diarypage/DatePage.jsx
+++ frontend/src/pages/diarypage/DatePage.jsx
@@ -1,11 +1,18 @@
+import { useParams } from "react-router";
 import Container from "../../components/common/Container";
 import Headerwb from "../../components/common/Headerwb";
 import styles from "./DatePage.module.css";
 
 export default function DatePage() {
+  const { diaryId } = useParams();
+
   return (
     <Container>
-      <Headerwb title={"날짜 상세ㅋㅋ"} />
+      <Headerwb title={`칵테일 ${diaryId}`} />
+
+      <div className={styles.background}>
+        <div className={styles.cover}></div>
+      </div>
     </Container>
   );
 }
