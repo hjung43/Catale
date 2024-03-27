781615452aff9cbaa1ceb2a562657f8d4efe0ddf
최소영 <tiger6453127@naver.com>
2024-03-27 16:29:50 +0900

    FEAT: 옵션 설정 기반 칵테일 검색기능 로직 구현

--- backend/src/main/java/com/catale/backend/domain/cocktail/repository/custom/CocktailRepositoryImpl.java
+++ backend/src/main/java/com/catale/backend/domain/cocktail/repository/custom/CocktailRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.catale.backend.domain.cocktail.dto.CocktailGetResponseDto;
 import com.catale.backend.domain.cocktail.dto.CocktailListResponseDto;
 import com.catale.backend.domain.cocktail.dto.CocktailSimpleInfoDto;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -54,6 +55,64 @@ public class CocktailRepositoryImpl implements CocktailRepositoryCustom {
                 .limit(page.getPageSize())
                 .fetch());
     }
+
+    @Override
+    public Optional<List<CocktailSimpleInfoDto>> searchByOption(int base, int alc, int sweet, int sour, int bitter, int sparkling, Pageable page) {
+        return Optional.ofNullable(query.select(Projections.constructor(CocktailSimpleInfoDto.class, cocktail))
+                .from(cocktail)
+                .where(eqBase(base), eqAlc(alc), eqSweet(sweet), eqSour(sour), eqBitter(bitter), eqSparkling(sparkling))
+                .offset(page.getOffset())
+                .limit(page.getPageSize())
+                .fetch());
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private BooleanExpression eqBase(int base) {
+        if(base == -1) {
+            return null;
+        }
+        return cocktail.base.eq(base);
+    }
+
+    private BooleanExpression eqAlc(int alc) {
+        if(alc == -1) {
+            return null;
+        }
+        return cocktail.alc.eq(alc);
+    }
+
+    private BooleanExpression eqSweet(int sweet) {
+        if(sweet == -1) {
+            return null;
+        }
+        return cocktail.sweet.eq(sweet);
+    }
+
+    private BooleanExpression eqSour(int sour) {
+        if(sour == -1) {
+            return null;
+        }
+        return cocktail.sour.eq(sour);
+    }
+
+    private BooleanExpression eqBitter(int bitter) {
+        if(bitter == -1) {
+            return null;
+        }
+        return cocktail.bitter.eq(bitter);
+    }
+
+    private BooleanExpression eqSparkling(int sparkling) {
+        if(sparkling == -1) {
+            return null;
+        }
+        return cocktail.sparking.eq(sparkling);
+    }
+
+
 
 
 }
