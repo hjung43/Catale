21c9943229ae7fa2bee96c7df660742ccf8bc02a
심규리 <jinry0034@naver.com>
2024-03-29 16:16:15 +0900

    FIX:닉네임 패턴 설정

--- backend/src/main/java/com/catale/backend/domain/member/dto/SignupRequestDto.java
+++ backend/src/main/java/com/catale/backend/domain/member/dto/SignupRequestDto.java
@@ -29,7 +29,7 @@ public class SignupRequestDto {
 
     @Pattern(regexp = "^[a-zA-Z0-9_]{3,10}$",
             message = "닉네임은 영문자, 숫자 및 언더바(_)를 포함할 수 있으며 3~10자 이내여야 합니다.")
-    @Schema(description = "닉네임", example = "Catale_2024")
+    @Schema(description = "닉네임", example = "Catale_1")
     private String nickname;
 
 }
