30e6c67d9fdf6a10eca0cdbb5b8903248258bc01
최소영 <tiger6453127@naver.com>
2024-03-29 10:39:46 +0900

    FEAT: 모델 훈련/테스트 데이터 생성

--- recommend-server/app/main.py
+++ recommend-server/app/main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 async def getTest():
     return {"message": "Test api 호출 완료"}
 
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     return crud.create_user(db=db, user=user)
+@app.post("/api/v1/recommend/{cocktailId}")
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
 
 # @app.post("/image/", response_model=schemas.ImageCreate)
 # def create_image(image: schemas.ImageCreate, db: Session = Depends(database.get_db)):
