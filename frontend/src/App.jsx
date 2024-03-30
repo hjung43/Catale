9f0f132181fb93c3fd67cf94eb673f6c8d9918af
최준호 <cojuna@naver.com>
2024-03-31 00:36:08 +0900

    FEAT : 서비스워커

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,17 @@ axios.interceptors.request.use(
     //return Promise.reject(error);
   }
 );
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service Worker registration failed", err);
+    });
+}
+
 export default function App() {
   useEffect(() => {
     function setScreenHeight() {
