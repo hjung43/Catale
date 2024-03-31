1ce0685d7c15083d1e5ba73ce0fd36d9dd54a21a
seom98 <seo_m98@naver.com>
2024-04-01 01:23:27 +0900

    FEAT: 칵테일 상세페이지, 결과페이지

---
+++ frontend/src/components/review/Review.jsx
@@ -0,0 +1,9 @@
+import styles from "./Review.module.css";
+
+import { useState, useEffect } from "react";
+
+export default function Review({ list }) {
+  console.log(list);
+
+  return <div></div>;
+}
