7a6369a8ddb50178cf990a226270f0d62698218f
유호정 <ys0403ab@naver.com>
2024-04-01 01:10:16 +0900

    FIX: 칵테일별 리뷰리스트 dto수정

--- backend/src/main/java/com/catale/backend/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/catale/backend/domain/review/controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.catale.backend.domain.member.entity.Member;
 import com.catale.backend.domain.member.service.MemberService;
 import com.catale.backend.domain.review.dto.ReviewGetRequestDto;
 import com.catale.backend.domain.review.dto.ReviewGetResponseDto;
+import com.catale.backend.domain.review.dto.ReviewListResponseDto;
 import com.catale.backend.domain.review.service.ReviewService;
 import com.catale.backend.global.format.code.ApiResponse;
 import com.catale.backend.global.format.response.ResponseCode;
@@ -34,10 +35,10 @@ public class ReviewController {
 
     @Operation(summary = "칵테일별 리뷰 전체 조회", description = "각 칵테일 별 전체 리뷰 조회")
     @GetMapping("/{cocktailId}")
-    public ResponseEntity<?> getReviews(@PathVariable Long cocktailId,
+    public ResponseEntity<?> getReviews(@PathVariable(name = "cocktailId") Long cocktailId,
                                         @PageableDefault(page = 0, size = 10) Pageable page){
 
-        List<ReviewGetResponseDto> reviewList = reviewService.getReviews(cocktailId, page);
+        List<ReviewListResponseDto> reviewList = reviewService.getReviews(cocktailId, page);
         return response.success(ResponseCode.REVIEW_LIST_FETCHED,reviewList);
     }
     @Operation(summary = "칵테일 리뷰 작성", description = "칵테일에 대한 리뷰 작성")
@@ -50,7 +51,7 @@ public class ReviewController {
     }
     @Operation(summary = "칵테일 리뷰 삭제", description = "칵테일 리뷰 삭제")
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<?> deletReview(@PathVariable Long reviewId){
+    public ResponseEntity<?> deletReview(@PathVariable(name = "reviewId") Long reviewId){
         Long id = reviewService.deleteReview(reviewId);
         return response.success(ResponseCode.REVIEW_DELETED);
     }
