e0ec270d11ff0e3707d6ff44e8784955418f051a
서만기 <seo_m98@naver.com>
2024-04-01 17:46:04 +0900

    FEAT: 결과페이지 모달

--- frontend/src/pages/mainpage/ResultPage.jsx
+++ frontend/src/pages/mainpage/ResultPage.jsx
@@ -10,9 +10,10 @@ import glass4 from "../../assets/glass/glass4.png";
 import glass5 from "../../assets/glass/glass5.png";
 import glass6 from "../../assets/glass/glass6.png";
 import glass7 from "../../assets/glass/glass7.png";
-import like from "../../assets/common/like.png";
+import diary from "../../assets/common/diary.png";
 import s from "classnames";
 import { mood1, mood2 } from "../mainpage/Emodata/Emotionthree";
+import close from "../../assets/common/close.png";
 import { useNavigate } from "react-router-dom";
 
 export default function ResultPage() {
@@ -20,6 +21,7 @@ export default function ResultPage() {
   const [emotions, setEmotions] = useState([]);
   const [list, setList] = useState([]);
   const [resultData, setResultData] = useState({});
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     const fetchCocktailToday = async () => {
@@ -69,6 +71,13 @@ export default function ResultPage() {
     [35, 45, 55],
     [25, 40, 55],
   ];
+  const mood = [
+    "많이 속상했던 날..",
+    "조금 속상했던 날..",
+    "그럭저럭인 날",
+    "기분 좋은 날!",
+    "정말 행복했던 날!!!",
+  ];
 
   const validGlassIndex =
     resultData.glass >= 0 && resultData.glass < num.length;
@@ -92,10 +101,7 @@ export default function ResultPage() {
           <br />
           딱맞는 칵테일을 찾았어요!
         </div>
-        <div
-          className={styles.card}
-          onClick={() => navigate(`/cocktail/${resultData.cocktailId}`)}
-        >
+        <div className={styles.card}>
           <div className={styles.cocktail}>
             <div
               className={styles.glass_cover}
@@ -107,6 +113,7 @@ export default function ResultPage() {
                 src={glasses[resultData.glass]}
                 alt="glass"
                 className={styles.glass}
+                onClick={() => navigate(`/cocktail/${resultData.cocktailId}`)}
               />
             </div>
             <div className={styles.name}>{resultData.name}</div>
@@ -119,7 +126,14 @@ export default function ResultPage() {
                   return <div>{mood1[index][j]}</div>;
                 })}
               </div>
-              <div></div>
+
+              <div
+                className={styles.cocktail_right}
+                onClick={() => setModal(true)}
+              >
+                <div>오늘의 일기</div>
+                <img src={diary} alt="diary" className={styles.icon2} />
+              </div>
             </div>
           </div>
         </div>
@@ -156,6 +170,43 @@ export default function ResultPage() {
               );
             })}
           </div>
+        </div>
+      </div>
+      <div
+        className={s(styles.blur, modal ? styles.active : styles.no)}
+        onClick={() => setModal(false)}
+      ></div>
+      <div className={s(styles.modal, !modal && styles.none)}>
+        <div className={styles.delete_top}>
+          <img
+            src={close}
+            alt="close"
+            className={styles.icon}
+            onClick={() => setModal(false)}
+          />
+          <div className={styles.delete_title}>오늘의 일기</div>
+          <div className={styles.icon}></div>
+        </div>
+        <div className={styles.delete_text}>
+          <div>{resultData.reason} 때문에</div>
+          <div>
+            {emotions.map((emo, i) => {
+              const index = (emo - (emo % 10)) / 10;
+              const j = emo % 10;
+              if (emotions.length - 1 > i) {
+                return <span>{mood2[index][j]} </span>;
+              } else {
+                return <span>{mood1[index][j]} </span>;
+              }
+            })}{" "}
+            <br />
+            감정을 느낀
+          </div>
+          <div className={s(styles[`mood${resultData.mood}`], styles.mood)}>
+            {mood[resultData.mood]}
+          </div>
+          <br />
+          <div>{resultData.comment}</div>
         </div>
       </div>
     </ContainerMain>
